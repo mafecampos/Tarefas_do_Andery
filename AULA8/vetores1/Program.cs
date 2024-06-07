269a7ee0652f498eb372bf846a7b47ac70204353
Maria Fernanda <131400153+mafecampos@users.noreply.github.com>
2024-06-07 16:47:01 -0300

    Vetores_exercicio1
    números pares e ímpares

---
+++ AULA8/vetores1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int[] nums = new int[10];
+        Console.WriteLine("Escolha dez números inteiros: ");
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int escolha;
+            escolha = int.Parse(Console.ReadLine());
+            nums[i] = escolha;
+            if (nums[i] % 2 == 0)
+            {
+                Console.WriteLine("Seu número é PAR!");
+            }
+            else
+            {
+                Console.WriteLine("Seu número é ÍMPAR!");
+            }
+        }
+    }
+}
